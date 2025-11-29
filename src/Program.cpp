8c4a2db19cd782c98e34a328cf24a4d88384c764
yuki_cjm <yuki_cjm@sjtu.edu.cn>
2025-11-29 16:59:18 +0800

    feat: Complete Program.cpp

--- src/Program.cpp
+++ src/Program.cpp
@@ -1,1 +1,50 @@
 // TODO: Imply interfaces declared in the Program.hpp.
+#include "Program.hpp"
+
+#include "Statement.hpp"
+#include "Recorder.hpp"
+#include "VarState.hpp"
+
+Program::Program() = default;
+
+void Program::addStmt(int line, Statement* stmt){
+    recorder_.add(line, stmt);
+}
+
+void Program::removeStmt(int line){
+    recorder_.remove(line);
+}
+
+void Program::run(){
+    programCounter_ = 0;
+    programCounter_ = recorder_.nextLines(programCounter_);
+    while(programCounter_ != -1){
+        execute(recorder_[programCounter_]->second);
+        programCounter_ = recorder_.nextLines(programCounter_);
+    }
+}
+
+void Program::list() const{
+    recorder_.printLines();
+}
+
+void Program::clear(){
+    recorder_.clear();
+    vars_.clear();
+}
+
+void Program::execute(Statement* stmt){
+    stmt->execute(vars_, *this);
+}
+
+int Program::getPC() const noexcept{
+    return programCounter_;
+}
+
+void Program::changePC(int line){
+    programCounter_ = line;
+}
+
+void Program::programEnd(){
+    exit(0);
+}
